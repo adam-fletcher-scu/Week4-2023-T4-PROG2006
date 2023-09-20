c18bc70413a4c27a0537cda8c25f534b4830c167
adam-fletcher-scu <adam.fletcher@scu.edu.au>
2023-09-20 17:02:19 +1000

    Added UI to each Page

--- Assets/Scripts/NavBehaviour.cs
+++ Assets/Scripts/NavBehaviour.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NavBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadMyScene(int scene)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(scene);
     }
 }
